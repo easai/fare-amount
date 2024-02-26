82bbb716dbf645f18c4c9e73ffcde859f1618067
easai <aksarih@gmail.com>
2024-02-25 23:50:32 -0500

    updated

--- script.sql
+++ script.sql
@@ -1,34 +1,61 @@
--- This query creates a view of the modified taxi fare of the trip whose tip amount is more than the average for the distance group.
-CREATE OR REPLACE VIEW
-  fare_tip_view AS
+-- This query calculates the average tip rates per distance and per fare amount within the fare amount group, and count the number of trips that exceeds the average by 30%.
 WITH
-  TipRate AS(
-  -- Create a CTE and calculate tip rates for each distance group. The distance group is defined by trip_distance divided by 10, rounded.
+  DistanceTip AS (
+  -- Calculates the average tip rates per distance.
   SELECT
     ROUND(trip_distance / 10) AS distance_group,
-    AVG(tip_amount/trip_distance) tip_rate
+    AVG(tip_amount/trip_distance) AS avg_tip_per_distance
   FROM
     `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2021`
   WHERE
     trip_distance > 0
   GROUP BY
-    distance_group )
--- In the main SELECT query, it selects vender_id, the modified fare_amount, tip_amount, and trip_distance. The query joins the TipRate table for the distance_group of the row and includes only the trip with tip_amount larger than the calculated tip amount.
+    distance_group ),
+  FareTip AS (
+  -- Calculates the average tip rates per fare amount.
+  SELECT
+    ROUND(fare_amount / 10) AS fare_group,
+    AVG(tip_amount/fare_amount) AS avg_tip_per_fare
+  FROM
+    `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2021`
+  WHERE
+    fare_amount >0
+  GROUP BY
+    fare_group ),
+  TipThreshold AS (
+  -- Selects trips with tip amount exceeding the average amount by 30%.
+  SELECT
+    vendor_id,
+    CASE
+        WHEN
+          tip_amount > (
+          -- Select the average tip amount per distance for the distance group.
+          SELECT
+            avg_tip_per_distance
+          FROM
+            DistanceTip
+          WHERE
+            distance_group = ROUND(trip_distance / 10) ) * trip_distance * 1.3
+    
+          OR 
+
+          tip_amount > (
+          -- Select the average tip amount per fare amount for the fare amount group.
+          SELECT
+            avg_tip_per_fare
+          FROM
+            FareTip
+          WHERE
+            fare_group = ROUND(fare_amount / 10) ) * fare_amount * 1.3 
+        THEN 1
+        ELSE 0
+    END
+    AS tip_threshold_exceeded
+  FROM
+    `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2021` )
+-- Count the total trips and the number of trips with exceeded tips
 SELECT
-  vendor_id,
-  CASE
-    WHEN trip_distance < 25 THEN CAST(fare_amount AS FLOAT64)
-  ELSE
-  CAST(SAFE_ADD(fare_amount, extra) AS FLOAT64)
-END
-  AS fare_amount,
-  CAST(tip_amount AS FLOAT64) AS tip_amount,
-  CAST(trip_distance AS FLOAT64) AS trip_distance
+  COUNT(*) AS total_trips,
+  COUNTIF(tip_threshold_exceeded = 1) AS tip_threshold_exceeded_count
 FROM
-  `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2021`
-JOIN
-  TipRate
-ON
-  distance_group = ROUND(trip_distance / 10) 
-WHERE
-  tip_amount > ( TipRate.tip_rate )*trip_distance
+  TipThreshold
